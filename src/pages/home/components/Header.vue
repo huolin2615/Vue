d201e8c663c13af9c952c7c3f3cfe5562dea6d13
huolin1 <337010902@qq.com>
2019-08-09 18:02:25 +0800

    图头+轮播图

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -1,8 +1,16 @@
 <template>
   <div class="header">
-    <div class="header-left">返回</div>
-    <div class="header-input">内容</div>
-    <div class="header-right">城市</div>
+    <div class="header-left">
+      <div class="iconfont back-icon">&#xe624;</div>
+    </div>
+    <div class="header-title">
+      <span class="iconfont">&#xe632;</span>
+      输入城市/景点/游玩主题
+      </div>
+    <div class="header-right">
+      城市
+      <span class="iconfont arrow-icon">&#xe64a;</span>
+    </div>
   </div>
 </template>
 
@@ -17,15 +25,32 @@ export default {
 
 <style lang="stylus" scoped>
   .header
-		  display:flex
-    height: .86rem
-				.header-left
-				  width: .64rem
-						float: left
-				.header-input
-				  flex: 1
-				.header-right
-				  width: .1.24rem
-						float: right
+    display flex
+    line-height: .88rem
+    background: #00bcd4
+    color: #fff
+    .header-left
+      width: .66rem
+      text-align: center
+      float: left
+      .back-icon
+        font-size: .34rem
+    .header-title
+      flex: 1
+      height: .66rem
+      line-height: .66rem
+      margin-top: .12rem
+      margin-left: .2rem
+      padding-left: .2rem
+      background: #fff
+      border-radius: .1rem
+      color: #ccc
+    .header-right
+      width: 1.24rem
+      float: right
+      text-align: center
+      .arrow-icon
+        font-size: .15rem
+        margin-left: -.03rem
 
 </style>
---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,96 @@
+<template>
+    <div class="icons">
+        <div class="icon">
+					<div class='icon-img'>
+					<img class='icon-img-content' src='http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png' />
+					<p>景点门票</p>
+					</div>
+				</div>
+				<div class="icon">
+					<div class='icon-img'>
+					<img class='icon-img-content' src='http://img1.qunarzz.com/piao/fusion/1804/ff/fdf170ee89594b02.png' />
+					<p>深圳必游</p>
+					</div>
+				</div>
+				<div class="icon">
+					<div class='icon-img'>
+					<img class='icon-img-content' src='http://img1.qunarzz.com/piao/fusion/1803/4d/a1eda1a2b8414302.png' />
+					<p>暑期夜场</p>
+					</div>
+				</div>
+				<div class="icon">
+					<div class='icon-img'>
+					<img class='icon-img-content' src='http://img1.qunarzz.com/piao/fusion/1803/50/26ffa31b56646402.png' />
+					<p>海洋馆</p>
+					</div>
+				</div>
+				<div class="icon">
+					<div class='icon-img'>
+					<img class='icon-img-content' src='http://img1.qunarzz.com/piao/fusion/1804/5a/13ceb38dcf262f02.png' />
+					<p>一日游</p>
+					</div>
+				</div>
+				<div class="icon">
+					<div class='icon-img'>
+					<img class='icon-img-content' src='http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20194/cba147cf6cfcea7109d0bff6aac6f626.png' />
+					<p>深圳动物园</p>
+					</div>
+				</div>
+				<div class="icon">
+					<div class='icon-img'>
+					<img class='icon-img-content' src='http://img1.qunarzz.com/piao/fusion/1803/a6/6d97515091789602.png' />
+					<p>世界之窗</p>
+					</div>
+				</div>
+				<div class="icon">
+					<div class='icon-img'>
+					<img class='icon-img-content' src='http://img1.qunarzz.com/piao/fusion/1803/b6/37560ece9c62b502.png' />
+					<p>东部华侨城</p>
+					</div>
+				</div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HomeIcons',
+    data () {
+        return {
+        }
+    }
+}
+</script>
+<style lang="stylus" scoped>
+	.icons
+		overflow: hidden
+		height: 0
+		padding-bottom: 50%
+		.icon
+			position: relative
+			overflow: hidden
+			float: left
+			width: 25%
+			height: 0
+			padding-bottom: 25%
+		.icon-img
+			position: absolute
+			top: 0
+			left: 0
+			right: 0
+			bottom: .44rem
+			box-sizing: border-box
+			padding: .1rem
+			text-align: center
+			.icon-img-content
+				display: block
+				margin: 0 auto
+				height: 100%
+		.icon-desc
+			position: absolute
+			left: 0
+			right: 0
+			botton: 0
+			height: .44rem
+			line-height: .44rem
+			color: #333
+</style>
